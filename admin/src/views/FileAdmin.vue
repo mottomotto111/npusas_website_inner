3e9d6b2135b28775d0c6dbfd8fe90692cbbbd56b
田文喆 <88436919+nanisone@users.noreply.github.com>
2022-03-08 23:24:27 +0800

    el-upload组件更新 文件上传基本实现

--- admin/src/views/FileAdmin.vue
+++ admin/src/views/FileAdmin.vue
@@ -2,17 +2,19 @@
     <div>
         课程名称<input type="text">
         <el-button type="primary" icon="el-icon-search">查找大纲</el-button>        
-        <el-upload
-            class="upload-file"
-            action="/doUpload"
-            drag multiple :headers="headers" :auto-upload="true"
-			 :file-list="fileList" :on-change="handleChange">
+            <el-upload
+                class="upload-demo"
+                drag
+                :action="url+uploadUrl"
+                accept=".txt"
+                multiple>
 
-            <i class="el-icon-upload"></i>
-            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-            <!--<div class="el-upload__tip" slot="tip">上传教学大纲</div>-->
-            <el-button type="primary" icon="el-icon-plus">上传大纲</el-button> 
-        </el-upload>
+                <i class="el-icon-upload"></i>
+                <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                <el-button type="primary" >上传文件</el-button>
+                <div class="el-upload__tip" slot="tip">只能上传txt文件，且不超过500kb</div>
+            </el-upload>
+
         <div class="pageGroup">
             <el-button size="small" id="pageprev">上一页</el-button>
             <el-button size="small" id="pageafter">下一页</el-button>
@@ -43,6 +45,9 @@ export default{
     name:"FileAdmin",
     data(){
         return{
+            url: "http://localhost:9090",
+            uploadUrl: "/good/upload",
+            
             fileForm:{
                 fileList:[file1,file2]
             }
